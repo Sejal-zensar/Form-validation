a0a52c9b16c2cc80cf28cbc9e53d612805bdc8ab
SEJAL <56817477+sejal122@users.noreply.github.com>
2022-05-04 12:46:25 +0530

    Add files via upload

---
+++ basicform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list'; 
+@Component({
+  selector: 'app-basicform',
+  templateUrl: './basicform.component.html',
+  styleUrls: ['./basicform.component.css']
+})
+export class BasicformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
